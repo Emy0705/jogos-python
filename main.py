ac9010af2eeeccff587bb953269b1c204d362a1d
Emy0705 <129624201+Emy0705@users.noreply.github.com>
2023-07-10 11:29:01 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,31 +6,22 @@ display = pygame.display.set_mode((1280, 720))
 fps = pygame.time.Clock()
 jogando = True
 
-rect_1 = pygame.Rect(300, 300, 100, 100)
 
 while jogando:
-
-    display.fill((0, 0, 0))
-    pygame.draw.rect(display, (255, 255, 255), rect_1)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jogando = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                if rect_1.y > 0:
-                    rect_1.y -= 5
-
-            if event.key == pygame.K_s:
-                if rect_1.y < 620:
-                    rect_1.y += 5
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print(event.pos)
+            pygame.draw.circle(display, (255, 255, 255), event.pos, 50)
+
+
+
+
+
 
 
     fps.tick(60)
     pygame.display.flip()
-
-
